cca52b1d8499a527558acbc9867969a6fffb0552
Salman-Soomro <40438433+Salman-Soomro@users.noreply.github.com>
2021-04-17 11:25:41 +0300

    Update Notification.cs

--- PeeReview/Models/Notification.cs
+++ PeeReview/Models/Notification.cs
@@ -2,6 +2,50 @@ namespace PeeReview.Models
 {
     public class Notification //TODO
     {
+    private string body;
+        private string head;
+        private Date date;
+        private User user;
+
+        public string getHead()
+        {       
+            return head;
+        }
+
+        private void setHead(string head)
+        {       
+            this.head = head;
+        }
+
+        public string getBody()
+        {       
+            return body;
+        }
+
+        private void setBody(string body)
+        {       
+            this.body = body;
+        }
+
+        public Date getDate()
+        {
+            return Date;
+        }
+
+        private void setDate(Date date)
+        {
+            this.date = date;
+        }
+
+        public User getSender()
+        {
+            return sender;
+        }
+
+        private void setSender(User sender)
+        {
+            this.sender = sender
+        }
         
     }
 }
