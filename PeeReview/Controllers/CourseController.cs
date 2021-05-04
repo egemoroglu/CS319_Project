d85edd9e145efe8c1f9c68c3f8a2394c25d8a3ea
yiğit emiray <yigit.dinc@ug.bilkent.edu.tr>
2021-05-04 11:34:30 +0300

    Course Updated

--- PeeReview/Controllers/CourseController.cs
+++ PeeReview/Controllers/CourseController.cs
@@ -1,52 +1,59 @@
 using PeeReview.Models;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace PeeReview.Controllers
 {
     public class CourseController : Controller
     {
+        List<Course> courses = new List<Course>();
+
         public ActionResult CreateProject()
         {
-            Course course = new Course("OOP","CS319", 
-                new Instructor("Infrna", "InSur", "tr","223"));
-            ViewBag.Message = course;
             return View();
         }
         public ActionResult DetailedCoursePageStudent() //merge all detailed
         {
-            Course course = new Course("OOP","CS319", 
-                new Instructor("Infrna", "InSur", "tr","223"));
-            ViewBag.Message = course;
             return View();
         }
 
+        public List<Course> RetrieveCourses()
+        {
+            return courses;
+        }
         public bool addCourse(Course courseToAdd)
         {
+            courses.Insert(courses.Count, courseToAdd);
             //Add to db
             Connector conn = new Connector("CourseTable");
             conn.addCourse(courseToAdd);
             return true;
         }
-
         public bool removeCourse(Course courseToRemove)
         {
+            courses.RemoveAt(GetIndex(courseToRemove));
             //remove from db
             //check db if it exists
             Connector conn = new Connector("CourseTable");
             conn.removeCourse(courseToRemove);
             return true;
         }
-  
+        public int GetIndex(Course course)
+        {
+            for (int i = 0; i < courses.Count; i++)
+            {
+                if (course.CourseCode == courses[i].CourseCode) return i;
+            }
+            return -1;
+        }
+
         public ActionResult Enroll()//merge all assignment
         {
             return View();
         }
-        
+
         public ActionResult GroupsPageStudent() //
-       {
-           Course course = new Course("OOP","CS319", 
-               new Instructor("Infrna", "InSur", "tr","223"));
-           ViewBag.Message = course;
+        {
             return View();
         }
     }
