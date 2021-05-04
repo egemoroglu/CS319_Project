ec52d0a75fe3c72425fd6fbbbc5a1c7f4300d61a
abdul <razoq1999@gmail.com>
2021-05-04 11:32:24 +0300

    UI even  more

--- PeeReview/Controllers/EvaluateController.cs
+++ PeeReview/Controllers/EvaluateController.cs
@@ -1,15 +1,28 @@
+using System.Collections.Generic;
 using System.Web.Mvc;
+using PeeReview.Models;
 
 namespace PeeReview.Controllers
 {
     public class EvaluateController : Controller
     {
         public ActionResult EvaluateOtherGroups()
+
         {
+            List<string> cri = new List<string>{"communication", "work"};
+            
+            Evaluation evaluation = new Evaluation(cri);
+            Group group = new Group("group victory",new Course("CS333","213",
+                new Instructor("du","mm","yy","123"))) ;
+            EvalauteViewModel evm = new EvalauteViewModel(evaluation, group);
+            ViewBag.Message = evm;
             return View();
         }
         public ActionResult EvaluateTeamMembers() //merge all detailed
         {
+            List<string> cri = new List<string>{"communication", "work"};
+            Evaluation evaluation = new Evaluation(cri);
+            ViewBag.Message = evaluation;
             return View();
         }
         
--- PeeReview/Models/EvalauteViewModel.cs
+++ PeeReview/Models/EvalauteViewModel.cs
@@ -2,7 +2,13 @@ namespace PeeReview.Models
 {
     public class EvalauteViewModel
     {
-        public Evaluation evaluation { get; set; }
+        public EvalauteViewModel(Evaluation eval, Group gr)
+        {
+            evaluation = eval;
+            group = gr;
+        }
 
+        public Evaluation evaluation { get; set; }
+        public Group group { get; set; }
     }
 }
