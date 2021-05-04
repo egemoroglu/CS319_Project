4f7e17074b386081270c4bca451ec2b5371d3707
yiğit emiray <yigit.dinc@ug.bilkent.edu.tr>
2021-05-04 03:13:14 +0300

    ADDED SIGN UP LOG IN

--- PeeReview/Controllers/SignUpController.cs
+++ PeeReview/Controllers/SignUpController.cs
@@ -13,5 +13,20 @@ namespace PeeReview.Controllers
             return View();
         }
   
+        /**
+         * Assuming data given by user is string form
+         */ 
+        public bool SignUp(string name, string surname, string studentId, string email, string password)
+        {
+            Connector conn = new Connector("StudentTable");
+            if (conn.userExist(email)) ; //HERE SEND UI ERROR MSG THIS EMAIL IS TAKEN
+            else
+            {
+                conn.addStudent(new Models.Student(name, surname, email, password, null));
+                //Check if email exist in system
+                return true;
+            }
+            return false; //USER COULDNOT ADDED!!!!!
+        }
     }
 }
